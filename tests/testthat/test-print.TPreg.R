ee33d7a6a2d07aea0c02571f826d269a29be62c6
Quentin Marcou <18257721+qmarcou@users.noreply.github.com>
2024-05-30 15:06:10 +0200

    Add tests for printing and plotting (failing)
    Plotting test fails when there's only an intercept

---
+++ tests/testthat/test-print.TPreg.R
@@ -0,0 +1,55 @@
+test_that("Printing TPreg objects does not throw any error", {
+  # FIXME use package's data to reduce code duplication
+  # Crude mortality binomial regression
+  set.seed(246852389)
+  n_ind <- 1e2
+  l_illness <- 1.0
+  l_death <- 0.1
+  s_time <- 0.5
+  synth_idm_data <- generate_uncensored_ind_exp_idm_data(
+    n_individuals = n_ind,
+    lambda_illness = l_illness,
+    lambda_death = l_death
+  )
+  # FIXME I should not have to have those
+  # Generate random age and sex labels
+  synth_idm_data <-
+    synth_idm_data %>% tibble::add_column(
+      sex = ifelse(rbinom(n_ind, 1, prob = .5), "male", "female"),
+      age = runif(n = n_ind, min = 50, max = 80)
+    )
+  for (transition in c("11", "all")) {
+    estimate <-
+      renewnetTPreg(~1, synth_idm_data,
+        ratetable = NULL,
+        rmap = NULL,
+        time_dep_popvars = NULL,
+        s = s_time,
+        t = 3.0,
+        by = n_ind / 10,
+        trans = transition,
+        link = "logit",
+        R = 1 # Number of bootstraps
+      )
+    testthat::expect_no_error(
+      print(estimate)
+    )
+
+    # Check with more than just an intercept
+    estimate <-
+      renewnetTPreg(~sex, synth_idm_data,
+        ratetable = NULL,
+        rmap = NULL,
+        time_dep_popvars = NULL,
+        s = s_time,
+        t = 3.0,
+        by = n_ind / 10,
+        trans = transition,
+        link = "logit",
+        R = 1 # Number of bootstraps
+      )
+    testthat::expect_no_error(
+      print(estimate)
+    )
+  }
+})
